57e98b116c5241777f71fee9c15ce3952f7c0b77
sjpeccoud <samuel.peccoud@gmail.com>
2020-07-02 15:30:22 -0600

    Create categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,15 +1,31 @@
 class CategoriesController < ApplicationController
+	before_action :set_category, only: [:new]
 
 	def new
 
+	end
+
+	def create
+		@category = Category.new(category_params)
+		if @category.save
+			flash[:notice] = "Category was successfully created"
+			redirect_to @category
+		else
+			render 'new'
+		end
 	end
 
 	def index
 
 	end
 
-	def show
+	private
 
+	def set_category
+		@category = Category.new
 	end
 
+	def category_params
+		params.require(:category).permit(:name)
+	end
 end
